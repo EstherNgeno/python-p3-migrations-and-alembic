ca14b97bd6b5295ec036749f55674bcf290c6f35
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-10-22 22:20:16 +0300

    final commit

---
+++ app/migrations/versions/ccd89f076dfd_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ccd89f076dfd
+Revises: 
+Create Date: 2024-10-22 22:04:50.525510
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ccd89f076dfd'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
